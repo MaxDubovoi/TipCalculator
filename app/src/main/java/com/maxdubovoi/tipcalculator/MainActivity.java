e8cdc0113e016bde785a004b9b133aad7121b32b
Max <maximdubovoi@gmail.com>
2017-01-04 22:56:41 +0200

    P151//Fix problem with TextView&EditText position Class's variables

--- app/src/main/java/com/maxdubovoi/tipcalculator/MainActivity.java
+++ app/src/main/java/com/maxdubovoi/tipcalculator/MainActivity.java
@@ -1,10 +1,20 @@
 package com.maxdubovoi.tipcalculator;
 
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Форматировщики денежных сумм и процентов
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static  final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+    private double billAmount = 0.0;
+    private double percent = 0.15;
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
